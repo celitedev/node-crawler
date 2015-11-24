6f7a97bbe526dedc9000a547c84cbe07a2550476
celitedev <iagilelite@gmail.com>
2015-11-24 20:08:47 +0100

    general working producer/consumer separation. Also fixes #1

--- utils.js
+++ utils.js
@@ -6,39 +6,40 @@ module.exports = {
 	URL_SEED_PREFIX: "url_seed",
 	URL_DONE_MASTER_PREFIX: "url_done_master",
 	URL_DONE_DETAIL_PREFIX: "url_done_detail",
-	init: function(argv) {
+	init: function(config) {
 
-		if (!argv.crawler) {
-			throw new Error("command-line arg 'crawler' not defined");
-		}
-		var crawlResultSchema;
+		config.source = config.source.toLowerCase();
+		config.type = config.type.toLowerCase();
+
+		var crawlerName = this.calculated.getCrawlerName(config.source, config.type);
+
 		try {
-			var crawlerSchemaPath = path.resolve(__dirname + "/crawlers/" + argv.crawler);
-			crawlSchema = require(crawlerSchemaPath);
+			var crawlerSchemaPath = path.resolve(__dirname + "/crawlers/" + crawlerName);
+			crawlConfig = require(crawlerSchemaPath);
 
-			if (crawlSchema.schema.requiresJS) {
+			if (crawlConfig.schema.requiresJS) {
 				throw new Error("requiresJS not supported atm, since we've not implemented caching + prxying yet");
 				// var phantom = require('x-ray-phantom');
 				// x.driver(phantom());
 			}
 
-			if (!argv.isSeeder) {
-				crawlResultSchema = _.extend(crawlSchema.schema.results.schema(argv.x), {
-					calcPageDone: function(el, cb) {
-						downloadedDetailPages++;
-						cb();
-					}
-				});
-			}
-
 		} catch (err) {
 			throw err;
 			// console.log(err);
-			// throw new Error("crawler not found: " + argv.crawler);
+			// throw new Error("crawler not found: " + config.crawler);
+		}
+
+		if (crawlConfig.entity.type.toLowerCase() !== config.type) {
+			throw new Error("crawler type doesn't match up with filename (type, filename): " +
+				crawlConfig.entity.type.toLowerCase() + ", " + crawlerName);
+		}
+		if (crawlConfig.source.name.toLowerCase() !== config.source) {
+			throw new Error("crawler source doesn't match up with filename (source, filename): " +
+				crawlConfig.source.name.toLowerCase() + ", " + crawlerName);
 		}
 
 		try {
-			var outputSchemaName = crawlSchema.entity.type.toLowerCase();
+			var outputSchemaName = crawlConfig.entity.type.toLowerCase();
 			var outputSchemaPath = path.resolve(__dirname + "/schemas/" + outputSchemaName);
 			outputschema = require(outputSchemaPath);
 		} catch (err) {
@@ -46,23 +47,23 @@ module.exports = {
 		}
 
 		return {
-			crawlSchema: crawlSchema,
-			crawlResultSchema: crawlResultSchema,
-			outputschema: outputschema
+			crawlConfig: crawlConfig,
+			outputMessageSchema: outputschema
 		};
 	},
 	queues: {
-		seedQueueName: "seedUrls"
+		seedUrlQueueName: "seedUrls",
+		seedJobQueueName: "seedJob"
 	},
-	addSeedJob: function(queue, batchId, crawlSchema, cb) {
-		return queue.create(this.queues.seedQueueName, {
+	addCrawlJob: function(queue, batchId, crawlConfig, url, cb) {
+		return queue.create(this.queues.seedUrlQueueName, {
 				crawlJobId: batchId,
 				taskid: uuid.v4(), //id of this specific mini batch
-				source: crawlSchema.source.name,
-				type: crawlSchema.entity.type,
-				url: crawlSchema.schema.seed.config.seedUrl,
+				source: crawlConfig.source.name,
+				type: crawlConfig.entity.type,
+				url: url,
 				created: new Date().toISOString(),
-				title: createSeedUrlTitle(crawlSchema.source.name, crawlSchema.entity.type, crawlSchema.schema.seed.config.seedUrl)
+				title: this.calculated.getSeedUrlTitle(crawlConfig.source.name, crawlConfig.entity.type, crawlConfig.schema.seed.config.seedUrl)
 			})
 			.ttl(100 * 1000) // fail job if not complete in 100 seconds
 			//fail means retry: return to queue to be picked up later. Allows us to mimic 'at-least-once'-semantics
@@ -72,10 +73,32 @@ module.exports = {
 			.removeOnComplete(true)
 			.save(cb || noop);
 	},
-};
+	calculated: {
+		getSeedUrlTitle: function(source, type, url) {
+			return source + ":" + type + "--" + url;
+		},
 
-function createSeedUrlTitle(source, type, url) {
-	return source + ":" + type + "--" + url;
-}
+		/**
+		 * Get name of crawler which is '<source>:<type>'
+		 *
+		 * Input either: 
+		 * - source + type
+		 * - seedUrlJob
+		 * 
+		 * @param  {[type]} source [description]
+		 * @param  {[type]} type   [description]
+		 * @return {[type]}        [description]
+		 */
+		getCrawlerName: function(source, type) {
+			var name;
+			if (_.isObject(source)) {
+				name = source.source + ":" + source.type;
+			} else {
+				name = source + ":" + type;
+			}
+			return name.toLowerCase();
+		}
+	}
+};
 
 function noop() {}
