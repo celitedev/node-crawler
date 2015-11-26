e4aad91346adaad3127ec6223f7659b99d8159d0
celitedev <iagilelite@gmail.com>
2015-11-26 20:39:48 +0100

    busy with schema validation. relates to #25

--- schemas/event.js
+++ schemas/event.js
@@ -2,5 +2,8 @@
 //LATER: this may define a schema we store in Confluent Schema or something (Avro schemas) 
 module.exports = {
 	name: "Event",
-	version: "0.1"
+	version: "0.1",
+	schema: {
+
+	}
 };
