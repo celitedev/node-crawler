a913a11aaf63d7e220ed8a5742ec15c7a127d83d
gebrits <gbrits@gmail.com>
2015-12-01 14:02:15 +0100

    relates to #25

--- schemas/abstract.js
+++ schemas/abstract.js
@@ -1,10 +1,5 @@
 var ZSchema = require("z-schema");
 
-// ZSchema.registerFormat("fillHello", function(obj) {
-// 	obj.hello = "world";
-// 	return true;
-// });
-
 //Abstract schema. 
 //Used to type-check the envelope. 
 module.exports = {
@@ -15,15 +10,79 @@ module.exports = {
 		"type": "object",
 		properties: {
 			id: {
-				desription: "autocreated id of message. type=uuid.v4",
+				description: "autocreated id of message. type=uuid.v4",
 				"type": "string",
-				// required: true
+
 			},
 			meta: {
+				description: "meta data of message including versions of schemas",
+				"type": "object",
+				properties: {
+					"crawl": {
+						"type": "object",
+						description: "specifies info on this crawl",
+						properties: {
+							"batchId": {
+								description: "the id of the entire batch run. Monononically increasing",
+								"type": "number",
+								"minimum": 0,
+							},
+							"jobId": {
+								description: "specific mini job this entity instance is part of. " +
+									"A batch contains multiple jobs",
+								"type": "string"
+							},
+							"createdAt": {
+								"format": "date-time",
+								description: "datestring (iso) signaling creation date of this message",
+								"type": "string"
+							},
+							"crawlVersion": {
+								"description": "version of the crawler/mapper. <source,type> specific",
+								"type": "string"
+							},
+							"typeVersion": {
+								description: "version of the outputMessage/ type, describing the payload.",
+								"type": "string"
+							}
+						},
+						"required": ["batchId", "jobId", "createdAt", "crawlVersion", "typeVersion"]
+					},
+				},
+				"required": ["crawl"]
+			},
+			identifiers: {
+				description: "attributes that together allow identification of source entity instance",
+				"type": "object",
+				properties: {
+					id: {
+						type: "string",
+						description: "id of entity instance as known by source"
+					},
+					url: {
+						"format": "uri",
+						type: "string",
+						description: "url of entity instance as known by source"
+					},
+					source: {
+						//TODO: enum
+						type: "string",
+						description: "name of source as known by kwhen"
+					},
+					type: {
+						//TODO: enum
+						type: "string",
+						description: "name of type mapped. Independent of source"
+					}
+				},
+				"required": ["id", "url", "source", "type"]
+			},
+			payload: {
+				description: "actual payload of message. " +
+					"THis is covered by a different entity-type specific schema",
 				"type": "object"
 			}
-		}
-		// "type": "object",
-		// "format": "fillHello"
+		},
+		required: ["id", "meta", "identifiers", "payload"]
 	}
 };
--- schemas/event.js
+++ schemas/event.js
@@ -4,6 +4,54 @@ module.exports = {
 	name: "Event",
 	version: "0.1",
 	schema: {
-
+		"title": "Event Schema",
+		"type": "object",
+		"description": "A representation of an event",
+		"required": ["dtstart", "summary"],
+		"properties": {
+			"dtstart": {
+				"format": "date-time",
+				"type": "string",
+				"description": "Event starting time"
+			},
+			"dtend": {
+				"format": "date-time",
+				"type": "string",
+				"description": "Event ending time"
+			},
+			"summary": {
+				"type": "string"
+			},
+			"location": {
+				"type": "string"
+			},
+			"url": {
+				"type": "string",
+				"format": "uri"
+			},
+			"duration": {
+				"format": "time",
+				"type": "string",
+				"description": "Event duration"
+			},
+			"rdate": {
+				"format": "date-time",
+				"type": "string",
+				"description": "Recurrence date"
+			},
+			"rrule": {
+				"type": "string",
+				"description": "Recurrence rule"
+			},
+			"category": {
+				"type": "string"
+			},
+			"description": {
+				"type": "string"
+			},
+			"geo": {
+				"$ref": "http: //json-schema.org/geo"
+			}
+		}
 	}
 };
