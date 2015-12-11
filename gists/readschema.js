6d1f57590e3834543b2b028c9776d66f74d385bc
gebrits <gbrits@gmail.com>
2015-12-11 16:24:04 +0100

    update schema generation stuff

--- gists/readschema.js
+++ gists/readschema.js
@@ -22,3 +22,55 @@ var schema = _.extend({}, type, {
 	})
 });
 // console.log(schema);
+
+
+
+//EXAMPLE FROM https://schema.org/Person
+			// {
+			//   "@context": "http://schema.org",
+			//   "@type": "Person",
+			//   "address": {
+			//     "@type": "PostalAddress",
+			//     "addressLocality": "Seattle",
+			//     "addressRegion": "WA",
+			//     "postalCode": "98052",
+			//     "streetAddress": "20341 Whitworth Institute 405 N. Whitworth"
+			//   },
+			//   "colleague": [
+			//     "http://www.xyz.edu/students/alicejones.html",
+			//     "http://www.xyz.edu/students/bobsmith.html"
+			//   ],
+			//   "email": "mailto:jane-doe@xyz.edu",
+			//   "image": "janedoe.jpg",
+			//   "jobTitle": "Professor",
+			//   "name": "Jane Doe",
+			//   "telephone": "(425) 123-4567",
+			//   "url": "http://www.janedoe.com"
+			// }
+
+//THIS SEEMS WRONG SINCE COLLEQUE IS NOT EXPLICITLY SET TO BE OF TYPE @ID
+//sO THIS IS BETTER? 
+var test = {
+  "@context": {
+		"@vocab": "http://schema.org/",
+		"colleague": { "@type": "@id" }
+	},
+  "@type": "Person",
+  "address": {
+    "@type": "PostalAddress",
+    "addressLocality": "Seattle",
+    "addressRegion": "WA",
+    "postalCode": "98052",
+    "streetAddress": "20341 Whitworth Institute 405 N. Whitworth"
+  },
+  "colleague": [
+    "http://www.xyz.edu/students/alicejones.html",
+    "http://www.xyz.edu/students/bobsmith.html"
+  ],
+  "email": "mailto:jane-doe@xyz.edu",
+  "image": "janedoe.jpg",
+  "jobTitle": "Professor",
+  "name": "Jane Doe",
+  "telephone": "(425) 123-4567",
+  "url": "http://www.janedoe.com"
+};
