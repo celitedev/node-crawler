031b75b05690dce9678a31bce0e52d3fe0d4d75b
gebrits <gbrits@gmail.com>
2015-12-13 17:38:23 +0100

    misc schema gen . relates to #50

--- gists/readschema.js
+++ gists/readschema.js
@@ -1,4 +1,4 @@
-var all = require("../schemas/schemaOrg/all.js");
+var all = require("../schemas/schemaOrg.js");
 var argv = require('yargs').argv;
 var _ = require("lodash");
 
@@ -21,56 +21,59 @@ var schema = _.extend({}, type, {
 		return _.pick(all.properties[propName], ["id", "ranges"]);
 	})
 });
-// console.log(schema);
+console.log(schema);
 
 
 
 //EXAMPLE FROM https://schema.org/Person
-			// {
-			//   "@context": "http://schema.org",
-			//   "@type": "Person",
-			//   "address": {
-			//     "@type": "PostalAddress",
-			//     "addressLocality": "Seattle",
-			//     "addressRegion": "WA",
-			//     "postalCode": "98052",
-			//     "streetAddress": "20341 Whitworth Institute 405 N. Whitworth"
-			//   },
-			//   "colleague": [
-			//     "http://www.xyz.edu/students/alicejones.html",
-			//     "http://www.xyz.edu/students/bobsmith.html"
-			//   ],
-			//   "email": "mailto:jane-doe@xyz.edu",
-			//   "image": "janedoe.jpg",
-			//   "jobTitle": "Professor",
-			//   "name": "Jane Doe",
-			//   "telephone": "(425) 123-4567",
-			//   "url": "http://www.janedoe.com"
-			// }
+// {
+//   "@context": "http://schema.org",
+//   "@type": "Person",
+//   "address": {
+//     "@type": "PostalAddress",
+//     "addressLocality": "Seattle",
+//     "addressRegion": "WA",
+//     "postalCode": "98052",
+//     "streetAddress": "20341 Whitworth Institute 405 N. Whitworth"
+//   },
+//   "colleague": [
+//     "http://www.xyz.edu/students/alicejones.html",
+//     "http://www.xyz.edu/students/bobsmith.html"
+//   ],
+//   "email": "mailto:jane-doe@xyz.edu",
+//   "image": "janedoe.jpg",
+//   "jobTitle": "Professor",
+//   "name": "Jane Doe",
+//   "telephone": "(425) 123-4567",
+//   "url": "http://www.janedoe.com"
+// }
 
 //THIS SEEMS WRONG SINCE COLLEQUE IS NOT EXPLICITLY SET TO BE OF TYPE @ID
 //sO THIS IS BETTER? 
-var test = {
-  "@context": {
-		"@vocab": "http://schema.org/",
-		"colleague": { "@type": "@id" }
-	},
-  "@type": "Person",
-  "address": {
-    "@type": "PostalAddress",
-    "addressLocality": "Seattle",
-    "addressRegion": "WA",
-    "postalCode": "98052",
-    "streetAddress": "20341 Whitworth Institute 405 N. Whitworth"
-  },
-  "colleague": [
-    "http://www.xyz.edu/students/alicejones.html",
-    "http://www.xyz.edu/students/bobsmith.html"
-  ],
-  "email": "mailto:jane-doe@xyz.edu",
-  "image": "janedoe.jpg",
-  "jobTitle": "Professor",
-  "name": "Jane Doe",
-  "telephone": "(425) 123-4567",
-  "url": "http://www.janedoe.com"
-};
+// var test = {
+// 	"@context": {
+// 		"@vocab": "http://schema.org/",
+// 		"colleague": {
+// 			"@type": "@id"
+// 		},
+// 		"@id": "id" //
+// 	},
+// 	"@type": "Person",
+// 	"address": {
+// 		"@type": "PostalAddress",
+// 		"addressLocality": "Seattle",
+// 		"addressRegion": "WA",
+// 		"postalCode": "98052",
+// 		"streetAddress": "20341 Whitworth Institute 405 N. Whitworth"
+// 	},
+// 	"colleague": [
+// 		"http://www.xyz.edu/students/alicejones.html",
+// 		"http://www.xyz.edu/students/bobsmith.html"
+// 	],
+// 	"email": "mailto:jane-doe@xyz.edu",
+// 	"image": "janedoe.jpg",
+// 	"jobTitle": "Professor",
+// 	"name": "Jane Doe",
+// 	"telephone": "(425) 123-4567",
+// 	"url": "http://www.janedoe.com"
+// };
---
+++ gists/testInherit.js
@@ -0,0 +1,26 @@
+var generatedSchemas = require("../schemas/schemaOrg/generate.js");
+var argv = require('yargs').argv;
+var _ = require("lodash");
+
+if (!argv.type) {
+	throw new Error("commandline --type required");
+}
+
+var typeName = argv.type;
+
+var type = generatedSchemas.types[typeName];
+
+if (!type) {
+	throw new Error("type not found for:" + typeName);
+}
+
+console.log(type);
+
+// var schema = _.extend({}, type, {
+// 	propertySchemas: _.map(type.properties, function(propName) {
+// 		console.log(generatedSchemas.properties[propName].ranges);
+// 		return _.pick(generatedSchemas.properties[propName], ["id", "ranges"]);
+// 	})
+// });
+
+// console.log(schema);
