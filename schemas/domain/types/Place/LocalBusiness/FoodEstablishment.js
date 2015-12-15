4033a90fa57158762e69194242419930b72daf54
gebrits <gbrits@gmail.com>
2015-12-15 23:50:35 +0100

    adding LocalBusinesses. A lot disable=true to focus on the important ones

---
+++ schemas/domain/types/Place/LocalBusiness/FoodEstablishment.js
@@ -0,0 +1,11 @@
+module.exports = {
+	properties: {
+		acceptsReservations: {},
+		menu: {},
+		servesCuisine: {
+			isMulti: true
+		},
+		//TBD: soft charactistics like `audience`
+		//See: https://github.com/schemaorg/schemaorg/issues/933
+	}
+};
