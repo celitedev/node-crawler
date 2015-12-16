1c1c9ebc1add887967bd9ab7792bd10c8bace2da
celitedev <iagilelite@gmail.com>
2015-12-16 20:29:58 +0100

    some tooling (in /gists) to check interdependencies between schemas. For manual inspection, but also used as valdation primitives when generating datamodel schemas

---
+++ schemas/gists/showSchemas.js
@@ -0,0 +1,84 @@
+var argv = require('yargs').argv;
+var _ = require("lodash");
+var colors = require("colors");
+
+
+if (!argv.type) {
+	throw new Error("commandline --type required");
+}
+
+var utils = require("../utils");
+var generatedSchemas = require("../createDomainSchemas.js");
+
+
+var typeName = argv.type;
+var type = generatedSchemas.types[typeName];
+var typeChain = _.uniq(_.clone(type.ancestors).concat(typeName));
+
+if (!type) {
+	throw new Error("type not found for:" + typeName);
+}
+
+var commands = ["typesIn", "typesOut", "schema"];
+
+var command = argv.command;
+if (!command || commands.indexOf(command) === -1) {
+	throw new Error("commandline --command required. Available options: " + commands.join(","));
+}
+
+
+switch (command) {
+	case "schema":
+		console.log(_.keys(type.properties));
+		break;
+	case "typesIn":
+		typesIn();
+		break;
+	case "typesOut":
+		typesOut();
+		break;
+}
+
+function propertiesReferencingType() {
+	var propNames = _.reduce(generatedSchemas.properties, function(arr, p, propName) {
+		if (p.ranges.indexOf(typeName) !== -1) {
+			arr.push(propName);
+		}
+		return arr;
+	}, []);
+	return propNames;
+}
+
+
+function typesIn() {
+
+	var results = {},
+		alreadyCovered = [],
+		typesInDagOrder = utils.getTypesInDAGOrder(generatedSchemas.types);
+
+	_.each(typesInDagOrder, function(tName) {
+
+		var t = generatedSchemas.types[tName];
+		_.each(t.properties, function(p, propName) {
+			var interSect = _.intersection(typeChain, p.ranges);
+			if (interSect.length) {
+				var key = tName + "." + propName;
+				var isAlreadyCovered = false;
+				_.each(t.supertypes, function(superName) {
+					if (alreadyCovered.indexOf(superName + "." + propName) !== -1) {
+						isAlreadyCovered = true;
+					}
+				});
+				if (!isAlreadyCovered) {
+					results[key] = interSect;
+				}
+				alreadyCovered.push(key);
+			}
+		});
+	});
+	console.log(results);
+}
+
+function typesOut() {
+
+}
