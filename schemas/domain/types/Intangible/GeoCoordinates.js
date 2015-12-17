47dee69a1e460d25f57a4d05f170caaf20a02f62
gebrits <gbrits@gmail.com>
2015-12-17 20:39:37 +0100

    changed GeoCoordinates schema based on static analysis. Relates to #60

--- schemas/domain/types/Intangible/GeoCoordinates.js
+++ schemas/domain/types/Intangible/GeoCoordinates.js
@@ -1,14 +1,12 @@
 module.exports = {
+	//other props like address, addressCountry, etc removed
+	//based on static analysis of topology (see #60)
+	//Basically: Place is the only type that references valueObject GeoCoordinates. 
+	//Since Place has a property PostalAddress which contains addres, addressCountry, etc. 
+	//there's absolutely zero need to have these properties on GeoCoordinates
 	properties: {
-		address: {},
-		addressCountry: {
-			writeFrom: "address.addressCountry"
-		},
 		elevation: {},
 		latitude: {},
-		longitude: {},
-		postalCode: {
-			writeFrom: "address.postalCode"
-		},
+		longitude: {}
 	}
 };
