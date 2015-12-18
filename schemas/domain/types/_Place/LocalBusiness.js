70c487d961b08f0f01fe1335910435d8679bd0fd
gebrits <gbrits@gmail.com>
2015-12-18 23:32:21 +0100

    fixes #76

--- schemas/domain/types/_Place/LocalBusiness.js
+++ schemas/domain/types/_Place/LocalBusiness.js
@@ -3,7 +3,6 @@
 // a particular branch of a restaurant chain, a branch of a bank, a medical practice, 
 // a club, a bowling alley, etc.
 module.exports = {
-	ancestors: ["Thing", "Place", "PlaceWithOpeninghours", "Organization"],
 	supertypes: ["PlaceWithOpeninghours", "Organization"],
 	properties: {
 		// branchCode: {}, //already on Place
