a770ea317edc18bfa10ba963c8c4ce51fca669b1
celitedev <iagilelite@gmail.com>
2015-12-19 00:49:15 +0100

    WIP #77

--- schemas/gists/global.js
+++ schemas/gists/global.js
@@ -53,79 +53,86 @@ function ambiguous(types) {
 
 function allDefined(types) {
 
-	if (argv.roots) {
-		console.log(("Going with overwritten roots as defined through --roots-commandline: " + argv.roots).yellow);
-	} else {
-		console.log(("Going with default roots as specified in Config: " + config.domain.roots.join(",")).yellow);
-	}
+	var out = {};
+	_.each(types, function(t) {
+		out[t.id] = _.pick(t, ['rootName']);
+	});
 
-	var roots = argv.roots ? argv.roots.split(",") : config.domain.roots;
-
-	var obj = utils.generateDAG(types);
-	var hierarchy = obj.hierarchy;
-
-	var out = {
-		errors: {
-			undef: [],
-			rootCovered: {
-				isAbstract: [],
-				isValueObject: []
-			},
-			abstractAndValueObject: []
-		},
-		isValueObject: [],
-		isAbstract: [],
-		roots: {
-			// array per root
-		}
-	};
-
-	walk(hierarchy);
-
-	function walk(tree, rootNameSuper) {
-		_.each(tree, function(struct, name) {
-			var node = generatedSchemas.types[name];
-			var rootName = rootNameSuper;
-			if (roots.indexOf(name) !== -1) {
-				//FACT: node defined as root
-				rootName = name;
-			}
-			if (rootName) {
-				//FACT: node is root or covered by root
-
-				//CHECK: should not define isAbstract
-				if (node.isAbstract) {
-					out.errors.rootCovered.isAbstract.push(name);
-					return;
-				}
-
-				//CHECK: should not define isValueObject
-				if (node.isValueObject) {
-					out.errors.rootCovered.isValueObject.push(name);
-					return;
-				}
-
-				var agg = out.roots[rootName] = out.roots[rootName] || [];
-				agg.push(name);
-
-			} else {
-				if (node.isAbstract && node.isValueObject) {
-					out.errors.abstractAndValueObject.push(name);
-				} else if (node.isAbstract) {
-					out.isAbstract.push(name);
-				} else if (node.isValueObject) {
-					out.isValueObject.push(name);
-				} else {
-					out.errors.undef.push(name);
-				}
-			}
-			//recurse
-			walk(struct, rootName);
-		});
-	}
-	if (argv.onlyShowErrors) {
-		console.log(("only showing errors").yellow);
-		out = out.errors;
-	}
 	console.log(JSON.stringify(out, null, 2));
+
+	// if (argv.roots) {
+	// 	console.log(("Going with overwritten roots as defined through --roots-commandline: " + argv.roots).yellow);
+	// } else {
+	// 	console.log(("Going with default roots as specified in Config: " + config.domain.roots.join(",")).yellow);
+	// }
+
+	// var roots = argv.roots ? argv.roots.split(",") : config.domain.roots;
+
+	// var obj = utils.generateDAG(types);
+	// var hierarchy = obj.hierarchy;
+
+	// var out = {
+	// 	errors: {
+	// 		undef: [],
+	// 		rootCovered: {
+	// 			isAbstract: [],
+	// 			isValueObject: []
+	// 		},
+	// 		abstractAndValueObject: []
+	// 	},
+	// 	isValueObject: [],
+	// 	isAbstract: [],
+	// 	roots: {
+	// 		// array per root
+	// 	}
+	// };
+
+	// walk(hierarchy);
+
+	// function walk(tree, rootNameSuper) {
+	// 	_.each(tree, function(struct, name) {
+	// 		var node = generatedSchemas.types[name];
+	// 		var rootName = rootNameSuper;
+	// 		if (roots.indexOf(name) !== -1) {
+	// 			//FACT: node defined as root
+	// 			rootName = name;
+	// 		}
+	// 		if (rootName) {
+	// 			//FACT: node is root or covered by root
+
+	// 			//CHECK: should not define isAbstract
+	// 			if (node.isAbstract) {
+	// 				out.errors.rootCovered.isAbstract.push(name);
+	// 				return;
+	// 			}
+
+	// 			//CHECK: should not define isValueObject
+	// 			if (node.isValueObject) {
+	// 				out.errors.rootCovered.isValueObject.push(name);
+	// 				return;
+	// 			}
+
+	// 			var agg = out.roots[rootName] = out.roots[rootName] || [];
+	// 			agg.push(name);
+
+	// 		} else {
+	// 			if (node.isAbstract && node.isValueObject) {
+	// 				out.errors.abstractAndValueObject.push(name);
+	// 			} else if (node.isAbstract) {
+	// 				out.isAbstract.push(name);
+	// 			} else if (node.isValueObject) {
+	// 				out.isValueObject.push(name);
+	// 			} else {
+	// 				out.errors.undef.push(name);
+	// 			}
+	// 		}
+	// 		//recurse
+	// 		walk(struct, rootName);
+	// 	});
+	// }
+	// if (argv.onlyShowErrors) {
+	// 	console.log(("only showing errors").yellow);
+	// 	out = out.errors;
+	// }
+	// console.log(JSON.stringify(out, null, 2));
 }
