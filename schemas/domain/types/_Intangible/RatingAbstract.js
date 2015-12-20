1e6d258f9f5444cef8968fbc7311ce136bae2a57
celitedev <iagilelite@gmail.com>
2015-12-20 18:55:54 +0100

    separated out aggregateRating from Rating and changed AggRating to valueobject #2

---
+++ schemas/domain/types/_Intangible/RatingAbstract.js
@@ -0,0 +1,12 @@
+module.exports = {
+	isAbstract: true,
+	isCustom: true,
+	supertypes: ["Intangible"],
+	properties: {
+		about: {}, //added bc 'itemReviewed'  aliases to this. 
+		itemReviewed: {},
+		bestRating: {},
+		ratingValue: {},
+		worstRating: {}
+	}
+};
