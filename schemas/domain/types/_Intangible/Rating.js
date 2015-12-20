23b08b99e57efc8fcc1535c47ecea432f13d2483
gebrits <gbrits@gmail.com>
2015-12-20 18:15:47 +0100

    remove 'itemOfRootType' property which relates to #75 and is superseded

--- schemas/domain/types/_Intangible/Rating.js
+++ schemas/domain/types/_Intangible/Rating.js
@@ -2,7 +2,6 @@ module.exports = {
 	properties: {
 		about: {}, //added bc 'itemReviewed'  aliases to this. 
 		itemReviewed: {},
-		itemOfRootType: {},
 		bestRating: {},
 		ratingValue: {},
 		worstRating: {}
