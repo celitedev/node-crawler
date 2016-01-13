a8c25d7b4610f99d51a96894ac1d6523991781b6
gebrits <gbrits@gmail.com>
2016-01-13 12:52:10 +0100

    keep track of all processed index urls using sorted set per crawler. Relates to #123

--- crawlers/eventful:event.js
+++ crawlers/eventful:event.js
@@ -41,6 +41,7 @@ module.exports = {
 		//template options: 
 		//- hash: hash of detail contents
 		//- headers: based on cache headers
+		//- db: check against saved SourceObject
 		dirtyCheckEntity: "hash",
 
 
