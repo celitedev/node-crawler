02d14caa9a6c85922f2833a49c9836fbe39c98c2
gebrits <gbrits@gmail.com>
2016-01-14 17:19:26 +0100

    fixes #140 (prev wrong commit) + wip #130

---
+++ schemas/domain/entities/index.js
@@ -0,0 +1,12 @@
+module.exports = function(generatedSchemas, r) {
+
+	var AbstractEntity = require("./AbstractEntity")(generatedSchemas, r);
+	var CanonicalEntity = require("./CanonicalEntity")(generatedSchemas, AbstractEntity, r);
+	var SourceEntity = require("./SourceEntity")(generatedSchemas, AbstractEntity, r);
+
+	return {
+		CanonicalEntity: CanonicalEntity,
+		SourceEntity: SourceEntity,
+	};
+
+};
