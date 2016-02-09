226a62378a093e6dec8d65f136ffd5bddfe85feb
gebrits <gbrits@gmail.com>
2016-02-09 19:47:06 +0100

    WIP

---
+++ vocabularies/vocabs/subtypes.js
@@ -0,0 +1,33 @@
+var _ = require("lodash");
+
+module.exports = function(generatedSchemas) {
+
+
+	var exports = {
+		type: "static",
+		sourceMappings: {
+
+			//mappings from sourceEntity-values -> controlled values
+			//It's logical to model these per Type since this is how crawlers are written
+			PlaceWithOpeninghours: {
+				//TBD
+			}
+		},
+		//all values (independent of type) including their aliases and parents 
+		vocabulary: {
+			//TBD
+		}
+	};
+
+
+	//Add all schema.org types
+	exports.sourceMappings.Thing = exports.sourceMappings.Thing || {};
+
+	_.each(_.keys(generatedSchemas.types), function(type) {
+		exports.sourceMappings.Thing[type] = type;
+		exports.vocabulary[type] = type;
+	});
+
+
+	return exports;
+};
