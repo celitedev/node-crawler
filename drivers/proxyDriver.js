151cee02b6b9934774c526a38be2aacd3d29b176
celitedev <iagilelite@gmail.com>
2016-05-02 19:31:11 +0200

    add cache - fixes #227

--- drivers/proxyDriver.js
+++ drivers/proxyDriver.js
@@ -2,10 +2,59 @@
  * Module Dependencies
  */
 
+var Promise = require("bluebird");
+var fs = Promise.promisifyAll(require("fs"));
+var mkdirp = Promise.promisify(require('mkdirp'));
+
 var _ = require("lodash");
 var superagent = require('superagent');
 var debugUrls = require('debug')('kwhen-crawler-urls');
 require('superagent-proxy')(superagent);
+
+var md5 = require('md5');
+var cacheUtils = {
+  dirDepth: 5,
+  charsPerDir: 2,
+  cachePrefixDir: "fileCache",
+  createHashPath: function (crawlerName, url) {
+    var hash = md5(url);
+    var path = "";
+    for (var i = 1; i <= cacheUtils.dirDepth * cacheUtils.charsPerDir; i++) {
+      path += hash.charAt(i);
+      if (i % cacheUtils.charsPerDir === 0) {
+        path += "/";
+      }
+    }
+    return cacheUtils.cachePrefixDir + "/" + crawlerName + "/" + path + hash;
+  },
+  get: function (hash) {
+    return fs.readFileAsync(hash, "utf8")
+      .then(function (dtoStr) {
+        var json;
+        try {
+
+          json = JSON.parse(dtoStr);
+        } catch (err) { //parse error: likely due to kill program while we were writing to file
+          json = null;
+        }
+        return json;
+      });
+  },
+  set: function (hash, dto) {
+    var dirPath = hash.substring(0, hash.lastIndexOf("/"));
+
+    return Promise.resolve()
+      .then(function () {
+        return mkdirp(dirPath);
+      })
+      .then(function () {
+        return fs.writeFileAsync(hash, JSON.stringify(dto), "utf8");
+      })
+      .then(function () {
+        return dto;
+      });
+  }
+};
 
 /**
  * Export `driver`
@@ -29,6 +78,8 @@ function driver(driverOpts) {
   var fn = function http_driver(ctx, fn) {
 
     var opts = ctx.opts;
+
+    var cachePath = cacheUtils.createHashPath(opts.name, ctx.url);
 
     //if opts.batchId exists, we're processing a DETAIL PAGE. 
     //This is not enforced in any way, but opts should be supplied by crawler on the correct place. 
@@ -86,54 +137,92 @@ function driver(driverOpts) {
       if (!driverOpts) {
         throw new Error("driveOpts not passed");
       }
-      var opts = driverOpts;
-      agent
-        .get(ctx.url)
-        .set(_.defaults(ctx.headers, opts.headers))
-        .timeout(opts.timeoutMS || 40000) //have a timeout. Seems by default superagent doesn't set one, which can lead to hands
-        .proxy(opts.proxy) //TOR
-        .on('error', function (err) {
-          if (err.code === "ECONNABORTED") {
-            if (this.res) { //sometimes this.res is not set yet. In that case we can't have a Z_BUF_ERROR... hopefully...
-              this.res.on("error", function (errInternal) {
-                if (errInternal.code !== "Z_BUF_ERROR") {
-                  console.log(("'Error on Superagent response again..Sigh. See #126").red);
-                  throw errInternal; //crash this shit
-                }
-                // See: #126
-                //We're ignoring Z_BUF_ERROR in case 'ECONNABORTED' was rootCause. 
-                //In that case we only report the root-cause error. 
-                //
-                //This is a terrible hack to ensure lib error doesn't crash our entire process.
-              });
-            }
-          }
 
-          return fn(err);
+      Promise.resolve()
+        .then(function conditionalFetchFromCache() {
+          if (!driverOpts.doCache) return;
+
+          return cacheUtils.get(cachePath)
+            .catch(function (err) {
+              if (err.code !== "ENOENT") throw err;
+              return null; //return null if not found;
+            });
         })
-        .end(function (err, res) {
-
-          //This includes request errors (4xx and 5xx) as well as node errors, which is what we want. 
-          //Result: Job fails in entirety and retry later. 
-          //This is preferred since we're not able to do partial retries (we don't want to)
-          //
-          //See https://github.com/lapwinglabs/x-ray-crawler/pull/1 for the opposite viewpoint
-          //which we need share for our particular usecase.
-          if (err) { //WAS: (err && !err.status) 
-            return fn(err);
+        .then(function (cachedDTO) {
+          if (cachedDTO) {
+            return cachedDTO;
           }
 
-          ctx.status = res.status;
-          ctx.set(res.headers);
+          var opts = driverOpts;
 
-          ctx.body = 'application/json' == ctx.type ? res.body : res.text;
+          return Promise.resolve()
+            .then(function () {
+              return new Promise(function (resolve, reject) {
+                agent
+                  .get(ctx.url)
+                  .set(_.defaults(ctx.headers, opts.headers))
+                  .timeout(opts.timeoutMS || 40000) //have a timeout. Seems by default superagent doesn't set one, which can lead to hands
+                  .proxy(opts.proxy) //TOR
+                  .on('error', function (err) {
+                    if (err.code === "ECONNABORTED") {
+                      if (this.res) { //sometimes this.res is not set yet. In that case we can't have a Z_BUF_ERROR... hopefully...
+                        this.res.on("error", function (errInternal) {
+                          if (errInternal.code !== "Z_BUF_ERROR") {
+                            console.log(("'Error on Superagent response again..Sigh. See #126").red);
+                            return reject(errInternal);
+                          }
+                          // See: #126
+                          //We're ignoring Z_BUF_ERROR in case 'ECONNABORTED' was rootCause. 
+                          //In that case we only report the root-cause error. 
+                          //
+                          //This is a terrible hack to ensure lib error doesn't crash our entire process.
+                        });
+                      }
+                    }
+                    return reject(err);
+                  })
+                  .end(function (err, res) {
 
-          stats.unzippedInBytes += ctx.body.length;
 
-          // update the URL if there were redirects
-          ctx.url = res.redirects.length ? res.redirects.pop() : ctx.url;
+                    //This includes request errors (4xx and 5xx) as well as node errors, which is what we want. 
+                    //Result: Job fails in entirety and retry later. 
+                    //This is preferred since we're not able to do partial retries (we don't want to)
+                    //
+                    //See https://github.com/lapwinglabs/x-ray-crawler/pull/1 for the opposite viewpoint
+                    //which we need share for our particular usecase.
+                    if (err) { //WAS: (err && !err.status) 
+                      return reject(err);
+                    }
+
+                    var dto = {
+                      status: res.status,
+                      headers: res.headers,
+                      body: 'application/json' == ctx.type ? res.body : res.text,
+                      url: res.redirects.length ? res.redirects.pop() : ctx.url
+                    };
+
+                    stats.unzippedInBytes += dto.body.length;
+
+                    resolve(dto);
+                  });
+              });
+            })
+            .then(function conditionalAddToCache(dto) {
+              if (!driverOpts.doCache) return dto;
+              return cacheUtils.set(cachePath, dto);
+            });
+        })
+        .then(function doneCB(dto) {
+
+          ctx.status = dto.status;
+          ctx.body = dto.body;
+          ctx.url = dto.url;
+          ctx.set(dto.headers);
 
           return fn(null, ctx);
+        })
+        .catch(function errorCB(err) {
+          return fn(err);
         });
     }
 
