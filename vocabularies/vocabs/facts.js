b1237bedcd070d8aecb220fbe262c76d3781deea
gebrits <gbrits@gmail.com>
2016-05-17 15:00:39 +0200

    update attribute cache in redis with inProcess cache which updates every 5 min + cache refresh

--- vocabularies/vocabs/facts.js
+++ vocabularies/vocabs/facts.js
@@ -7,13 +7,48 @@ module.exports = function (generatedSchemas) {
     type: "static",
     sourceMappings: {
 
-      //NYC
+      //NYC.com
       PlaceWithOpeninghours: {
-        "chinese": "chinese"
+
+        //cuisine
+        "chinese": "chinese",
+        "indian/pakistani": ["indian", "pakistani"],
+        "sandwich shops": ["sandwich shops"],
+        "barbecue": ["bbq"],
+        "ice cream &amp; frozen yogurt": ["ice"],
+        "american (regional)": "american",
+        "american (new)": "american",
+        "southern style": ["southern", "american"],
+
+
+        //ambiance
+        "business casual<br>casual": ["business casual", "casual"],
+
+        //payment
+        "visa only": ["visa"],
+        "mastercard only": ["mastercard"],
+        "all major": ["all major cards"],
       }
     },
     vocabulary: {
-      "chinese": ["chinese", "eastern"]
+      "southern": ["southern"],
+      "american": ["american"],
+      "chinese": ["chinese"],
+      "indian": ["indian"],
+      "pakistani": ["pakistani"],
+      "sandwich shops": ["sandwiches"],
+      "bbq": ["barbecue", "bbq", "grill"],
+      "ice": ["ice cream", "frozen yoghurt"],
+
+      //ambiance
+      "business casual": ["business casual", "urban chique"],
+      "casual": ["casual"],
+
+
+      //payment
+      "all major cards": ["visa", "mastercard", "all major cards", "all major"],
+      "visa": "visa",
+      "mastercard": "mastercard"
     }
   };
 
