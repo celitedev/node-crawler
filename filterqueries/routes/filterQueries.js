93044dfb525a1c8bbbb50b9ddff4b2d16b5a08f1
celitedev <iagilelite@gmail.com>
2016-05-17 18:10:15 +0200

    chunk combination utils

--- filterqueries/routes/filterQueries.js
+++ filterqueries/routes/filterQueries.js
@@ -275,19 +275,116 @@ function createFilterQuery(command) {
   return filterQuery;
 }
 
+var chunkUtils = {
+
+  //Allow regex on one or more properties. Supplied as a map
+  //Only those parts are returned for which all properties match corresponding regex
+  filter: function (parts, filterObj) {
+
+    //make regex
+    _.each(filterObj, function (v, k) {
+      filterObj[k] = new RegExp(v, "g");
+    });
+
+    return _.filter(parts, function (part) {
+
+      var result = _.compact(_.map(filterObj, function (regex, key) {
+        return !!part[key].match(regex);
+      }));
+
+      return result.length === _.size(filterObj);
+
+    });
+  },
+  getParts: function (currentChunk, sChunk) {
+
+    var depth = 0;
+    var startPartAt = 0;
+
+    sChunk = sChunk.trim();
+    currentChunk.path = sChunk;
+
+    var part;
+    for (var i = 0; i < sChunk.length; i++) {
+      var c = sChunk.charAt(i);
+      if (c === '[') {
+        depth++;
+      } else if (c === ']') {
+        depth--;
+        if (!depth) {
+
+          part = {
+            type: "chunk",
+            path: sChunk.substring(startPartAt, i + 1).trim(),
+          };
+          currentChunk.parts = currentChunk.parts || [];
+          currentChunk.parts.push(chunkUtils.getParts(part, part.path.substring(1, part.path.length - 1)));
+
+          startPartAt = i + 1;
+        }
+      } else if ((c === " " || i === sChunk.length - 1) && !depth) {
+
+        part = sChunk.substring(startPartAt, i + 1).trim();
+
+        if (part.trim()) { //don't add spaces
+
+          if (!startPartAt) { //we're at start. Possibly the chunk type
+            currentChunk.chunkType = part;
+          } else {
+            var wordTag = part.split("/");
+            part = {
+              type: "tag",
+              tag: wordTag[1],
+              word: wordTag[0],
+              path: part,
+              text: wordTag[0]
+            };
+            currentChunk.parts = currentChunk.parts || [];
+            currentChunk.parts.push(part);
+          }
+        }
+
+        startPartAt = i;
+      }
+    }
+
+    currentChunk.text = _.reduce(currentChunk.parts, function (sOut, part) {
+      return sOut + " " + part.text;
+    }, "").trim();
+
+    //more sensible order for human consumption
+    var curPath = currentChunk.path;
+    delete currentChunk.path;
+    currentChunk.path = curPath;
+
+    return currentChunk;
+  },
+};
 
 var middleware = {
   superSweetNLP: function superSweetNLP(req, res, next) {
     if (req.body.isNew && !req.type && req.body.question !== undefined) {
 
       var tags = NLPRules.getTags(req.body.question);
-      var chunks = NLPRules.getChunks(tags);
+      var sChunk = NLPRules.getChunks(tags);
 
+      var currentChunk = {
+        type: "top"
+      };
+
+
+      var chunks = chunkUtils.getParts(currentChunk, sChunk);
+
+      var nps = chunkUtils.filter(chunks.parts, {
+        chunkType: "VP"
+      });
+      console.log("VP", nps);
 
 
       res.json({
         tags: tags,
-        chunks: chunks
+        chunks: sChunk,
+        tree: chunks
       });
 
 
