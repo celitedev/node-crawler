d9fd59c203beb4013b84076f5302c400351cc143
celitedev <iagilelite@gmail.com>
2016-06-02 18:10:38 +0200

    separate out reloadNLP

--- server/routes/filterQueries.js
+++ server/routes/filterQueries.js
@@ -120,7 +120,6 @@ module.exports = function (command) {
   var r = command.r;
   var erdEntityTable = r.table(domainUtils.statics.ERDTABLE);
   var cacheUtils = command.cacheUtils;
-  var redisClient = command.redisClient;
 
   var filterQueryUtils = require("../utils")(generatedSchemas, r);
 
@@ -205,55 +204,6 @@ module.exports = function (command) {
       });
   });
 
-
-
-  //Update Redis NLP Cache from ES
-  app.post("/reloadNLPCache", function (req, res) {
-
-    var promiseMap = _.reduce(roots, function (agg, root) {
-
-      var cachePropMap = cacheUtils.cachePropertyMap;
-
-      var searchQuery = {
-        index: "kwhen-" + root.toLowerCase(),
-        type: 'type1',
-        body: {
-          "size": 0,
-          "aggs": _.reduce(cachePropMap, function (agg, v, k) {
-            agg[k] = {
-              "terms": {
-                "field": v.esField,
-                "size": 0
-              }
-            };
-            return agg;
-          }, {})
-        }
-      };
-
-      //cache into redis buckets
-      agg[root] = Promise.resolve()
-        .then(function () {
-          return command.esClient.search(searchQuery);
-        })
-        .then(function (result) {
-          return Promise.map(_.keys(cachePropMap), function (k) {
-            var redisKey = "cache-" + root.toLowerCase() + "-" + k;
-            var supportedKeys = _.pluck(result.aggregations[k].buckets, "key");
-            return redisClient.setAsync(redisKey, supportedKeys.join(","));
-          });
-        });
-
-      return agg;
-
-    }, {});
-
-    return Promise.props(promiseMap)
-      .then(function (resultJSON) {
-        cacheUtils.updateInProcessCaches();
-        res.json(resultJSON);
-      });
-  });
 
   app.get('/', function (req, res) {
     res.send('Use POST silly');
---
+++ server/routes/reloadNLP.js
@@ -0,0 +1,59 @@
+var _ = require("lodash");
+var Promise = require("bluebird");
+var roots = require("../../schemas/domain/_definitions/config").domain.roots;
+
+module.exports = function (command) {
+
+  var app = command.app;
+  var cacheUtils = command.cacheUtils;
+  var redisClient = command.redisClient;
+
+  //Update Redis NLP Cache from ES
+  app.post("/reloadNLPCache", function (req, res) {
+
+    var promiseMap = _.reduce(roots, function (agg, root) {
+
+      var cachePropMap = cacheUtils.cachePropertyMap;
+
+      var searchQuery = {
+        index: "kwhen-" + root.toLowerCase(),
+        type: 'type1',
+        body: {
+          "size": 0,
+          "aggs": _.reduce(cachePropMap, function (agg, v, k) {
+            agg[k] = {
+              "terms": {
+                "field": v.esField,
+                "size": 0
+              }
+            };
+            return agg;
+          }, {})
+        }
+      };
+
+      //cache into redis buckets
+      agg[root] = Promise.resolve()
+        .then(function () {
+          return command.esClient.search(searchQuery);
+        })
+        .then(function (result) {
+          return Promise.map(_.keys(cachePropMap), function (k) {
+            var redisKey = "cache-" + root.toLowerCase() + "-" + k;
+            var supportedKeys = _.pluck(result.aggregations[k].buckets, "key");
+            return redisClient.setAsync(redisKey, supportedKeys.join(","));
+          });
+        });
+
+      return agg;
+
+    }, {});
+
+    return Promise.props(promiseMap)
+      .then(function (resultJSON) {
+        cacheUtils.updateInProcessCaches();
+        res.json(resultJSON);
+      });
+  });
+
+};
