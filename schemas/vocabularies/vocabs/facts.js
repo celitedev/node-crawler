a8f3fb1b3b69bf4275dee2e637369eed5e8b4b7a
celitedev <iagilelite@gmail.com>
2016-06-02 22:13:20 +0200

    vocabularies - regression snafu

--- schemas/vocabularies/vocabs/facts.js
+++ schemas/vocabularies/vocabs/facts.js
@@ -2,48 +2,76 @@ var _ = require("lodash");
 
 module.exports = function (generatedSchemas) {
 
+  var genreVocab = require("./genre");
 
   var exports = {
     type: "static",
     sourceMappings: {
 
-      //NYC.com
+      //copy from genre vocab
+      Movie: genreVocab.sourceMappings.Movie,
+
+
       PlaceWithOpeninghours: {
 
-        //cuisine
-        "chinese": "chinese",
-        "indian/pakistani": ["indian", "pakistani"],
-        "sandwich shops": ["sandwich shops"],
-        "barbecue": ["bbq"],
-        "ice cream &amp; frozen yogurt": ["ice"],
-        "american (regional)": "american",
-        "american (new)": "american",
-        "southern style": ["southern", "american"],
-
+        //////////////
+        //NYC.com
 
         //ambiance
-        "business casual<br>casual": ["business casual", "casual"],
+        "business casual": ["business casual"],
+        "casual": ["casual"],
+        "fine dining": ["finedining"],
 
         //payment
         "visa only": ["visa"],
         "mastercard only": ["mastercard"],
         "all major": ["all major cards"],
-      }
+      },
+
+      FoodEstablishment: {
+
+        /////////////
+        //NYC.com
+
+        //NOTE: LOTS OF RAW CUISINES HERE THAT WE DON'T MAP BUT PASSTHROUGH VERBATIM
+
+        //cuisine
+        "indian/pakistani": ["indian", "pakistani"],
+        "sandwich shops": ["sandwichshop"],
+        "barbecue": ["bbq"],
+        "ice cream &amp; frozen yogurt": ["ice"],
+        "american (regional)": "american",
+        "american (new)": "american",
+        "southern style": ["southern", "american"],
+        "fish &amp; chips": "fishchips",
+        "food trucks": "foodtruck",
+        "hamburger &amp; hot dog stands": "hamburgerstand",
+        "hot dog": "hamburgerstand",
+        "wine &amp; cheese": "wineandcheese",
+        "sushi bars": "sushi",
+        "soup &amp; salad": "soupsalad",
+      },
     },
     vocabulary: {
+      //cuisine
       "southern": ["southern"],
       "american": ["american"],
-      "chinese": ["chinese"],
       "indian": ["indian"],
       "pakistani": ["pakistani"],
-      "sandwich shops": ["sandwiches"],
+      "sandwichshop": ["sandwich shop", "sandwiches"],
       "bbq": ["barbecue", "bbq", "grill"],
-      "ice": ["ice cream", "frozen yoghurt"],
+      "ice": ["ice", "ice cream", "frozen yoghurt"],
+      "fishchips": ["fish and chips"],
+      "foodtruck": ["foodtruck", "food truck"],
+      "hamburgerstand": ["hamburgers", "hamburger stand", "hotdogs", "hotdog stand"],
+      "wineandcheese": ["wine and cheese", "wine", "cheese"],
+      "sushi": ["sushi bar", "sushi"],
+      "soupsalad": ["soup and salad"],
 
       //ambiance
       "business casual": ["business casual", "urban chique"],
       "casual": ["casual"],
-
+      "finedining": ["fine dining", "chique"],
 
       //payment
       "all major cards": ["visa", "mastercard", "all major cards", "all major"],
@@ -53,14 +81,36 @@ module.exports = function (generatedSchemas) {
   };
 
 
-  //Add all schema.org types
-  exports.sourceMappings.Thing = exports.sourceMappings.Thing || {};
+  //Merge genre vocabulary which is relic from movies
+  exports.vocabulary = _.merge(exports.vocabulary, genreVocab.vocabulary);
 
-  _.each(_.keys(generatedSchemas.types), function (type) {
-    type = type.toLowerCase();
-    exports.sourceMappings.Thing[type] = type; //this goes for everything
-    exports.vocabulary[type] = _.uniq((exports.vocabulary[type] || []).concat([type]));
-  });
+  ////////////////////
+  //DEPRECATE AFTER TESTING THIS ISN'T NEEDED.
+  //
+  // //Add all schema.org types on level of roots
+  // //This results in, say exports.vocabulary.PlaceWithOpeninghours to contain all subtypes lowercased
+  // //
+  // //NOTE: THIS IS A DIRECT COPY OF SUBTYPES. 
+  // //THIS IS **TEMPORARY***. WE USE THIS FOR NOW BECAUSE WE SIMPLY ONLY 
+  // //MATCH ON FACTS, INSTEAD OF SEPARATING OUT PER ATTRIBUTE. THIS MAKES FOR FAR EASIER
+  // //FILTERCONTEXT CONSTRUCTION. 
+  // //
+  // //DOWNSIDES: 
+  // //- ES GETS POPULATED WITH LESS DESCRIPTIVE FACTS, WHICH ALSO ADDS TO BLOAT
+  // _.each(generatedSchemas.types, function (val, typeName) {
+
+  //   var rootsOfType = _.intersection([typeName].concat(val.ancestors), roots);
+  //   if (!rootsOfType.length) return; //not avail for toplevel such as Things and Intangible
+
+  //   typeName = typeName.toLowerCase();
+
+  //   var rootName = rootsOfType[rootsOfType.length - 1];
+  //   var rootMap = exports.sourceMappings[rootName] = exports.sourceMappings[rootName] || {};
+  //   rootMap[typeName] = typeName;
+  //   exports.vocabulary[typeName] = _.uniq((exports.vocabulary[typeName] || []).concat([typeName]));
+  // });
+
+
 
   return exports;
 };
--- schemas/vocabularies/vocabs/subtypes.js
+++ schemas/vocabularies/vocabs/subtypes.js
@@ -1,5 +1,7 @@
 var _ = require("lodash");
 
+var roots = require("../../domain/_definitions/config").domain.roots;
+
 module.exports = function (generatedSchemas) {
 
 
@@ -7,31 +9,226 @@ module.exports = function (generatedSchemas) {
     type: "static",
     sourceMappings: {
 
-      //NYC
+      Thing: {
+        "event": "event",
+      },
+
+      CreativeWork: {
+        //adding type=Movie alias
+        "movie": "movie",
+      },
+
+      Event: {
+        "screeningevent": "screeningevent"
+      },
+
       PlaceWithOpeninghours: {
+
+        /////////////////////////
+        //NYC.com RESTAURANTS
+        //incomplete
         "restaurant": "restaurant",
         "bistro": ["bistro", "restaurant"],
         "bar": "bar",
+        "bars": "bar",
+        "pub": "pub",
         "club": "club",
-      }
+
+        //Fandango movietheater
+        "movietheater": "movietheater",
+
+        ////////////////////////
+        //NYC.com attractions
+        "touristattraction": "attraction",
+        "museums": ["attraction", "museum"],
+        "theaters": ["attraction", "theater"],
+        "venues": ["attraction", "venue"],
+        "historic city sites": ["attraction", "historic"],
+        "parks": ["attraction", "park"],
+        "art galleries": ["artgallery"],
+        "universities": ["university"],
+        "classical music & opera": ["theater", "opera", "concerthall"],
+        "beaches": "beach",
+        "zoos": "zoo",
+        "amusement parks": "amusementpark",
+        "botanical gardens": "botanicalgarden",
+
+        //skipped since not denoting subtype
+        //parades & festivals, tv show tapings, kid friendly, dance
+
+
+        ////////////////////////
+        //NYC.com nightlife
+        "local hang out": ["hangout"],
+        "wine bar": ["winebar", "bar"],
+        "dive bar": ["divebar", "bar"],
+        "hotel bars": ["hotelbar", "bar"],
+        "live music": ["livemusic", "bar"],
+        "sports bar": ["sportsbar", "bar"],
+        "after work": ["afterwork", "bar"],
+        "danceg club": ["danceclub", "club"],
+        "lounge": ["lounge", "bar"],
+        "gay & lesbian": ["gay", "bar"],
+        "gay": ["gay", "bar"],
+        "comedy club": ["comedyclub"],
+        "billiards": "billiards",
+        "jazz": ["jazzclub", "club"],
+        "karaoke": ["karaoke", "bar"],
+        "whiskey bars": ["whiskeybar", "bar"],
+        "cabaret & revue": ["theater", "cabaret"],
+        "cigar bar": ["cigarbar", "bar"],
+        "cocktail bar": ["cocktailbar", "bar"],
+        "mixology": ["cocktailbar", "bar"],
+        "djs": ["dj", "danceclub", "bar"],
+        "strip club": ["stripclub", "nightclub"],
+        "piano bar": ["pianobar", "bar"],
+
+
+        //////////////////////
+        ///THIS IS WEIRD. LET'S NOT DO THIS. 
+        ///THE TERM 'ROMANTIC' IS NOT ENOUGH TO MAKE IT A BAR. 
+        ///IT *IS* FOR NYC.COM BUT THIS IS *NOT* GENERALLY APPLICABLE.
+        //These are just tagged as bar (in addition to added as genre)
+        // "upscale": "bar",
+        // "hot spot": "bar",
+        // "outdoor seating": "bar",
+        // "romantic": "bar",
+        // "singles": "bar",
+      },
+
+      FoodEstablishment: {
+
+
+        /////////////
+        //NYC.com
+
+        //!! NOTE: nyc.com cuisines are facts as well as subtypes.
+        //NOTE: LOTS OF RAW CUISINES HERE THAT WE DON'T MAP BUT PASSTHROUGH VERBATIM
+
+        //cuisine
+        "indian/pakistani": ["indian", "pakistani"],
+        "sandwich shops": ["sandwichshop"],
+        "barbecue": ["bbq"],
+        "ice cream &amp; frozen yogurt": ["ice"],
+        "american (regional)": "american",
+        "american (new)": "american",
+        "southern style": ["southern", "american"],
+        "fish &amp; chips": "fishchips",
+        "food trucks": "foodtruck",
+        "hamburger &amp; hot dog stands": "hamburgerstand",
+        "hot dog": "hamburgerstand",
+        "wine &amp; cheese": "wineandcheese",
+        "sushi bars": "sushi",
+        "soup &amp; salad": "soupsalad",
+      },
     },
+
     //all values (independent of type) including their aliases and parents 
+    //IMPORTART FOR SUBTYPES: use the schema.org subtype names as keys
     vocabulary: {
+
+      /////////////////
+      ///TYPE ALIASES
+      "event": ["event", "happening"],
+
+      "movie": ["movie", "film"],
       "restaurant": ["restaurant", "eatery", "diner"],
+      "movietheater": ["movietheater", "movie theater"],
+      "screeningevent": ["screeningevent", "movie screening"],
+
+      ///////////////////
+      ///restaurants
       "bistro": ['bistro'],
       "bar": ["bar"],
-      "club": "club"
+      "pub": "pub",
+      "club": "club",
+
+
+      //cuisine (NYC.com)
+      "southern": ["southern"],
+      "american": ["american"],
+      "indian": ["indian"],
+      "pakistani": ["pakistani"],
+      "sandwichshop": ["sandwich shop", "sandwiches"],
+      "bbq": ["barbecue", "bbq", "grill"],
+      "ice": ["ice", "ice cream", "frozen yoghurt"],
+      "fishchips": ["fish and chips"],
+      "foodtruck": ["foodtruck", "food truck"],
+      "hamburgerstand": ["hamburgers", "hamburger stand", "hotdogs", "hotdog stand"],
+      "wineandcheese": ["wine and cheese", "wine", "cheese"],
+      "sushi": ["sushi bar", "sushi"],
+      "soupsalad": ["soup and salad"],
+
+
+      //////////////////
+      //attractions
+      "attraction": ["attraction", "tourist attraction"],
+      "museum": "museum",
+      "theater": "theater",
+      "venue": "venue",
+      "historic": ["historic", "site", "historic site"],
+      "park": "park",
+      "artgallery": ["art gallery", "art galleries", "gallery", "galleries"], //complex plural
+      "university": ["university", "universities"], //complex plural
+      "opera": ["opera", "opera house"],
+      "concerthall": ["concerthall", "concert hall"],
+      "beach": "beach",
+      "zoo": "zoo",
+      "amusementpark": ["amusement park", "amusementpark"],
+      "botanicalgarden": "botanical garden",
+
+
+      /////////////
+      ///nightlife
+      "hangout": ["hangout", "hang out"],
+      "winebar": ["wine bar", "bar"],
+      "divebar": ["dive bar", "bar"],
+      "hotelbar": ["hotel bar", "bar"],
+      "lounge": ["lounge", "bar"],
+      "livemusic": ["live music", "bar"],
+      "sportsbar": ["sports bar", "bar"],
+      "afterwork": ["afterwork", "bar", "after work"],
+      "danceclub": ["danceclub", "club", "dance club", "clubbing"],
+      "gay": ["gay", "gay lesbian", "gay and lesbian", "lgbt", "gay bar"],
+      "comedyclub": ["comedyclub", "comedy club", "comedy"],
+      "billiards": "billiards",
+      "jazzclub": ["jazz", "jazzclub", "jazz club"],
+      "karaoke": ["karaoke", "karaoke bar", "bar", "karaoke club"],
+      "whiskeybar": ["whiskey bar"],
+      "cabaret": ["cabaret", "revue"],
+      "cigarbar": ["cigarbar", "cigar", "cigars", "cigar bar", "bar"],
+      "cocktailbar": ["mixology", "cocktailbar", "cocktail bar", "cocktails", "cocktail"],
+      "dj": ["dj"],
+      "stripclub": ["stripclub", "strip club", "vice"],
+      "nightclub": ["nightclub", "night club"],
+      "pianobar": ["pianobar", "piano bar", "bar"],
+
+
+
+      ///////////////
+      ///events
+      //adding for coverage in frontend (even if no entities yet)
+      "danceevent": ["dance", "clubbing", "concert"],
+      "festival": ["festival", "concert"],
+      "musicevent": ["musicevent", "concert", "music event"],
+
     }
   };
 
 
-  //Add all schema.org types
-  exports.sourceMappings.Thing = exports.sourceMappings.Thing || {};
+  //Add all schema.org types on level of roots
+  //This results in, say exports.vocabulary.PlaceWithOpeninghours to contain all subtypes lowercased
+  _.each(generatedSchemas.types, function (val, typeName) {
 
-  _.each(_.keys(generatedSchemas.types), function (type) {
-    type = type.toLowerCase();
-    exports.sourceMappings.Thing[type] = type; //this goes for everything
-    exports.vocabulary[type] = _.uniq((exports.vocabulary[type] || []).concat([type]));
+    var rootsOfType = _.intersection([typeName].concat(val.ancestors), roots);
+    if (!rootsOfType.length) return; //not avail for toplevel such as Things and Intangible
+
+    typeName = typeName.toLowerCase();
+
+    var rootName = rootsOfType[rootsOfType.length - 1];
+    var rootMap = exports.sourceMappings[rootName] = exports.sourceMappings[rootName] || {};
+    rootMap[typeName] = typeName;
+    exports.vocabulary[typeName] = _.uniq((exports.vocabulary[typeName] || []).concat([typeName]));
   });
 
   return exports;
