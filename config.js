d449ba52fd45a98692e865c87cbb2b1fb176e46f
celitedev <iagilelite@gmail.com>
2016-06-18 14:22:49 +0200

    multi env config

--- config.js
+++ config.js
@@ -35,6 +35,38 @@ var config = {
       buffer: 50,
       max: 1000
     }
+  },
+  prod_remote: {
+    redis: {
+      host: "localhost",
+      port: 6379
+    },
+
+    //https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/configuration.html
+    elasticsearch: {
+      host: 'http://ec2-54-236-244-71.compute-1.amazonaws.com:9200',
+      // log: 'trace',
+      apiVersion: "2.1",
+      // maxSockets: 10, //default
+      // defer: function() { //overwrite how ES makes promises
+      //  return Promise.defer();
+      // }
+    },
+
+    rethinkdb: {
+      pool: true, //default = true
+      //true is interesting:  When true, the driver will regularly pull data from the table server_status 
+      //to keep a list of updated hosts, default false
+      //TBD: check if useul to put to true.
+      discovery: false,
+      db: "kwhen", //default databse if non is mentioned
+      servers: [{
+        host: 'ec2-52-87-199-237.compute-1.amazonaws.com',
+        port: 28015
+      }],
+      buffer: 50,
+      max: 1000
+    }
   }
 };
 
@@ -43,36 +75,6 @@ if(!config[env]){
   throw new Error("Config error");
 }
 
-console.log(("Starting server in ") + ("'" + env + "'-mode").yellow);
-module.exports = {
-  redis: {
-    host: "localhost",
-    port: 6379
-  },
+console.log(("Starting with config: ") + ("'" + env + "'-mode").yellow);
 
-  //https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/configuration.html
-  elasticsearch: {
-    host: 'http://ec2-52-91-153-20.compute-1.amazonaws.com:9200',
-    // log: 'trace',
-    apiVersion: "2.1",
-    // maxSockets: 10, //default
-    // defer: function() { //overwrite how ES makes promises
-    // 	return Promise.defer();
-    // }
-  },
-
-  rethinkdb: {
-    pool: true, //default = true
-    //true is interesting:  When true, the driver will regularly pull data from the table server_status 
-    //to keep a list of updated hosts, default false
-    //TBD: check if useul to put to true.
-    discovery: false,
-    db: "kwhen", //default databse if non is mentioned
-    servers: [{
-      host: 'localhost',
-      port: 28015
-    }],
-    buffer: 50,
-    max: 1000
-  }
-};
+module.exports = config[env];
