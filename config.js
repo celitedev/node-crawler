9c9ee7824d006b64bb86d910b2a995fbf56c7507
celitedev <iagilelite@gmail.com>
2016-10-04 22:39:21 -0400

    re #374 add staging-remote configuration

--- config.js
+++ config.js
@@ -92,6 +92,43 @@ var config = {
       port: 9000
     }
   },
+  staging_remote: {
+    redis: {
+      host: "localhost",
+      port: 6379
+    },
+
+    proxy: {
+      host: "ec2-52-45-105-133.compute-1.amazonaws.com",
+      port: 5566
+    },
+
+    //https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/configuration.html
+    elasticsearch: {
+      host: 'http://ec2-23-22-187-201.compute-1.amazonaws.com:9200',
+      // log: 'trace',
+      apiVersion: "2.2",
+      // maxSockets: 10, //default
+      // defer: function() { //overwrite how ES makes promises
+      //  return Promise.defer();
+      // }
+    },
+
+    rethinkdb: {
+      pool: true, //default = true
+      //true is interesting:  When true, the driver will regularly pull data from the table server_status
+      //to keep a list of updated hosts, default false
+      //TBD: check if useul to put to true.
+      discovery: false,
+      db: "kwhen", //default database if none is mentioned
+      servers: [{
+        host: 'ec2-23-22-187-201.compute-1.amazonaws.com',
+        port: 28015
+      }],
+      buffer: 50,
+      max: 1000
+    }
+  },
   prod_remote: {
     redis: {
       host: "localhost",
