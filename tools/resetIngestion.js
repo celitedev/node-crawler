91515a5dc540f67990fb7cce7851eff178d76c73
Jim Edelstein <jim@platphormcorp.com>
2017-02-01 23:34:11 -0500

    make cleanup tool work better

--- tools/resetIngestion.js
+++ tools/resetIngestion.js
@@ -14,22 +14,32 @@ return Promise.resolve()
 
     return new Promise((resolve, reject) => {
       console.log(`Removing all data except sourceEntities, reset sourceEntities to new`);
-      tableSourceEntities.replace(r.row.without([{_state:{modifiedMakeRefs: true}},{_state:{modifiedAndDirty: true}},{_state:{modifiedMatch: true}}])).run().then(()=>{
+      tableSourceEntities.replace(r.row.without([
+        {_state:{modifiedMakeRefs: true}},
+        {_state:{modifiedAndDirty: true}},
+        {_state:{modifiedMatch: true}},
+        '_refToSourceRefIdMap',
+        '_refNormIds',
+        '_refs'
+      ])).run().then(()=>{
+        console.log('source entities cleaned?!');
         tableRefNorms.delete().run().then(()=>{
+          console.log("Ref Norms Removed");
           tableCanonicalEntities.delete().run().then(()=>{
+            console.log("Canonical Entities Removed");
             tableErd.delete().run().then(()=>{
-              console.log("DONE!");
+              console.log("ERD Removed, DONE!  Don't forget to run renewERDMapping before running ingestion");
               return resolve();
             })
           })
         })
       })
     })
-    .catch(function (err) {
-      throw err;
-    })
-    .finally(function () {
-      console.log("QUITTING");
-      r.getPoolMaster().drain(); //quit
-    });
+      .catch(function (err) {
+        throw err;
+      })
+      .finally(function () {
+        console.log("QUITTING");
+        r.getPoolMaster().drain(); //quit
+      });
   });
